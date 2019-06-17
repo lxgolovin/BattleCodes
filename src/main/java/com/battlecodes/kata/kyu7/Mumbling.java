092e7b4af9bb460d93f1fe2485a88bcc2f8b9902
lxgolovin <lxgolovin@gmail.com>
2019-06-17 16:47:27 +0300

    Mumbling task added

---
+++ src/main/java/com/battlecodes/kata/kyu7/Mumbling.java
@@ -0,0 +1,54 @@
+package com.battlecodes.kata.kyu7;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * This time no story, no theory. The examples below show you how to write function accum:
+ *
+ * Examples:
+ *
+ * accum("abcd") -> "A-Bb-Ccc-Dddd"
+ * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ * accum("cwAt") -> "C-Ww-Aaa-Tttt"
+ * The parameter of accum is a string which includes only letters from a..z and A..Z.
+ */
+public class Mumbling {
+
+    /**
+     * repeat method to repeat string several times
+     * @param count number of times to repeat
+     * @param with string to be repeated
+     * @return new string
+     */
+    private static String repeat(int count, String with) {
+        StringBuilder sb = new StringBuilder(with.toUpperCase());
+        for(int j=0; j<count; j++) {
+            sb.append(with.toLowerCase());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Accumulates string according to task
+     * @param str input string to be changed
+     * @return string after modification
+     */
+    static String accum(final String str) {
+        String[] result = str.split("");
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = repeat(i, result[i]);
+        }
+
+        return String.join("-", result);
+    }
+
+    public static String accumStream(String s) {
+        return IntStream.range(0, s.length())
+                .mapToObj(i -> IntStream.range(0, i + 1)
+                        .mapToObj(i1 -> i1 == 0 ? String.valueOf(s.charAt(i)).toUpperCase() : String.valueOf(s.charAt(i)).toLowerCase())
+                        .collect(Collectors.joining())
+                ).collect(Collectors.joining("-"));
+    }
+}
---
+++ src/test/java/com/battlecodes/kata/kyu7/MumblingTest.java
@@ -0,0 +1,33 @@
+package com.battlecodes.kata.kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This time no story, no theory. The examples below show you how to write function accum:
+ *
+ * Examples:
+ *
+ * accum("abcd") -> "A-Bb-Ccc-Dddd"
+ * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ * accum("cwAt") -> "C-Ww-Aaa-Tttt"
+ * The parameter of accum is a string which includes only letters from a..z and A..Z.
+ */
+class MumblingTest {
+
+    @Test
+    void MumblingAccumulString() {
+        assertEquals(Mumbling.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        assertEquals(Mumbling.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        assertEquals(Mumbling.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        assertEquals(Mumbling.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        assertEquals(Mumbling.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+
+        assertEquals(Mumbling.accumStream("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        assertEquals(Mumbling.accumStream("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        assertEquals(Mumbling.accumStream("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        assertEquals(Mumbling.accumStream("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        assertEquals(Mumbling.accumStream("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+}
