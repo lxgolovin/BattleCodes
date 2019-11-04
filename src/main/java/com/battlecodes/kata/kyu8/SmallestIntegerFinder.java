9bbf81cffb154b839585c6c032cd97a8d6c93c52
Alexander <lxgolovin@gmail.com>
2019-11-04 23:22:49 +0300

    more kyu8

---
+++ src/main/java/com/battlecodes/kata/kyu8/SmallestIntegerFinder.java
@@ -0,0 +1,27 @@
+package com.battlecodes.kata.kyu8;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Given an array of integers your solution should find the smallest integer.
+ *
+ * For example:
+ *
+ *     Given [34, 15, 88, 2] your solution will return 2
+ *     Given [34, -345, -1, 100] your solution will return -345
+ *
+ * You can assume, for the purpose of this kata, that the supplied array will not be empty.
+ */
+class SmallestIntegerFinder {
+    private SmallestIntegerFinder() {}
+
+    static int findSmallestInt(int[] args) {
+        return Arrays.stream(args).sorted().findFirst().orElse(0);
+
+    }
+
+    static int findSmallestIntStream(int[] args) {
+        return IntStream.of(args).min().orElse(0);
+    }
+}
